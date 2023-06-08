0975d00f11a240cbe5936881e3a5b8276dbf3373
Vikas Soni <sonivikas142@gmail.com>
2023-06-08 11:44:31 +0530

    add home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Work from "./components/Work"
 
 function App() {
   return <>
   <Header />
+  <Home />
+  <Work />
   </>
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,102 @@
+import React, { useRef } from "react";
+import { animate, motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
+import me from "../assets/logo.png";
+
+const Home = () => {
+
+  const clientCount = useRef(null);
+  const projectCount = useRef(null);
+
+  const animations = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      },
+    },
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+  };
+
+  const clientCountAnimation = ()=>{
+    animate(0,100, {
+      duration:1,
+      onUpdate:(v)=>(clientCount.current.textContent = v.toFixed())
+    });
+  };
+
+  const projectCountAnimation = ()=>{
+    animate(0,500, {
+      duration:1,
+      onUpdate:(v)=>(projectCount.current.textContent = v.toFixed())
+    });
+  };
+
+  return (
+    <div id="home">
+      <section>
+        <div>
+          <motion.h1 {...animations.h1}>
+            Hi, I am <br /> Vikas Soni
+          </motion.h1>
+
+          <Typewriter
+            options={{
+              strings: ["A Coder", "A Developer", "An Engineer"],
+              autoStart: true,
+              loop: true,
+              cursor: "",
+              wrapperClassName: "typewriterpara",
+            }}
+          />
+
+          <div>
+            <a href="mailto:sonivikas142@gmail.com">Hire Me</a>
+            <a href="#work">
+              Projects
+              <BsArrowUpRight />
+            </a>
+          </div>
+          <article>
+            <p>
+              +<motion.span ref={clientCount} whileInView={clientCountAnimation}>100</motion.span>
+            </p>
+            <span>Clients Worldwide</span>
+          </article>
+          <aside>
+            <article>
+              <p>
+                +<motion.span ref={projectCount} whileInView={projectCountAnimation}>500</motion.span>
+              </p>
+              <span> Projects Done</span>
+            </article>
+            <article data-special>
+              <p>Contact</p>
+              <span>sonivikas142@gmail.com</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+        <img src={me} alt="vikas" />
+      </section>
+      <BsChevronDown />
+    </div>
+  );
+};
+
+export default Home;
